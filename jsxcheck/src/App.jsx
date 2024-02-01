49989f61722a3bfc595d9b6c284b4d2d18f2ef01
rachidfall12 <rchdfall@gmail.com>
2024-02-01 22:02:36 +0000

    'yeah'

--- jsxcheck/src/App.jsx
+++ jsxcheck/src/App.jsx
@@ -1,17 +1,32 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from 'react';
 import './App.css'
 import Carte from './Components/carte/carte'
 
 
+const firstName = prompt("Dougalal seu tour aytia :");
+
+const isFirstNameProvided = firstName !== null && firstName.trim() !== "";
+
+
 
 function App() {
-  
+ 
   return (
-    <div>
-     <Carte />
+    <div className='rambo'>
+       <div>
+      <p className='p1'lass>Hello, {isFirstNameProvided ? firstName : "there"}!</p>
+      </div>
+        <div>
+       <Carte />
+       </div>
+       <div>
+      {isFirstNameProvided && (
+        <img  width={'250px'} src="https://i.pinimg.com/564x/8d/17/10/8d1710fe39eccfe75ab40b7ef9ac99af.jpg" alt="Votre Image" />
+      )}
+        </div>
     </div>
-    
-  )
+  );
 }
 export default App
 
